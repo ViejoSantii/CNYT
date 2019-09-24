aa5dde2ba1400cfc97714ac165f96d5c0eb9d7ab
ViejoSantii <46783804+ViejoSantii@users.noreply.github.com>
2019-09-23 22:31:10 -0500

    Add files via upload

---
+++ EXP.py
@@ -0,0 +1,75 @@
+from sys import stdin
+import math
+import unittest
+def adicion(c1, c2):
+     a1 = c1 [0]; a2 = c2 [0]
+     b1 = c1 [1]; b2 = c2 [1]
+     x = a1 + a2
+     y = b1 + b2
+     z = (x, y)
+     return z
+def producto(c1, c2):
+     a1 = c1 [0]; a2 = c2 [0]
+     b1 = c1 [1]; b2 = c2 [1]
+     x = (a1 * a2) - (b1 * b2)
+     y = (a1 * b2) + (a2 * b1)
+     z = (x, y)
+     return z
+
+def sustraccion(c1, c2):
+     a1 = c1 [0]; a2 = c2 [0]
+     b1 = c1 [1]; b2 = c2 [1]
+     x = a1 - a2
+     y = b1 - b2
+     z = (x, y)
+     return z
+
+def Mproducto (A, B):
+    filasB = len(B)
+    columnasA = len(A[0])
+    if filasB == columnasA:
+        filas = len(A)
+        columnas = len(B[0])
+        matriz = [[(0, 0)] * columnas for x in range(filas)]
+        for i in range(0, filas):
+            for j in range(0, columnas):
+                for k in range(0, len(B)):
+                    m = producto(A[i][k], B[k][j])
+                    n = matriz[i][j]
+                    matriz[i][j] = (m[0]+n[0], m[1]+n[1])
+        return matriz
+    else:
+        print("La dimensión es incorrecta")
+        return False
+
+def Mtraspuesta (matriz):
+    filas = len(matriz)
+    columnas = len(matriz[0])
+    return [[matriz[j][i] for j in range(filas)] for i in range(columnas)]
+
+
+
+
+def canicas(m, v, N):
+    '''m = [[(c1, c2)], [(c1, c2)],...,[(c1, c2)]], 
+       v = [(c1, c2), (c1, c2),...,(c1, c2)]
+       N = int;
+    (m ** N) * v = V;
+    matriz, vector, int ---> vector'''
+    mat_n = m
+    for i in range (1, N-1):
+        n = Mproducto (mat_n, m)
+        mat_n = n
+    return (Mproducto (mat_n, Mtraspuesta(v)))
+
+def bullets(m, v, N):
+    '''m = [[(c1, c2)], [(c1, c2)],...,[(c1, c2)]], 
+       v = [(c1, c2), (c1, c2),...,(c1, c2)]
+       N = int;
+    (m ** N) * v = V;
+    matriz, vector, int ---> vector'''
+    mat_n = m
+    for i in range (1, N-1):
+        n = Mproducto (mat_n, m)
+        mat_n = n
+    return (Mproducto (mat_n, Mtraspuesta(v)))
